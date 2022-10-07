3d65ed29114a384dcb2857c1f1241b6055b73243
Юлия Заплетнюк <uliazapletnuk@MacBook-Air-Ulia.local>
2022-10-07 09:03:36 +0300

    master commit 2

--- labor02.cpp
+++ labor02.cpp
@@ -12,15 +12,15 @@ int main()
 {
     double Pi = 4 * atan(1.);
     double a;
-    double z1;
+    //double z1;
     double z2;
 
     cout << "a= "; cin >> a;
-    z1 =  (cos((3.0/8)*Pi-(a/4))*cos((3.0 / 8) * Pi - a / 4))-(cos((11.0/8)*Pi+(a/4))* cos((11.0 / 8)*Pi + (a / 4)));
+    //z1 =  (cos((3.0/8)*Pi-(a/4))*cos((3.0 / 8) * Pi - a / 4))-(cos((11.0/8)*Pi+(a/4))* cos((11.0 / 8)*Pi + (a / 4)));
     z2 = (sqrt(2) / 2) * (sin(a / 2));
 
     cout << endl;
-    cout << "z1= " << z1 << endl;
+    //cout << "z1= " << z1 << endl;
     cout << "z2 = " << z2 << endl;
 
     cin.get();
